9810460a3b7774e72d565d81f2b0dd4bd83f9588
bicerf <furkanbicerresmi@gmail.com>
2021-09-16 16:26:39 +0300

    require file path fixed

--- Models/Book.php
+++ Models/Book.php
@@ -1,5 +1,5 @@
 <?php
-require_once "DB.class.php";
+require_once "../DB.class.php";
 
 class Book extends DB
 {
@@ -12,3 +12,4 @@ class Book extends DB
     }
 }
 ?>
+
--- Models/Post.php
+++ Models/Post.php
@@ -1,5 +1,5 @@
 <?php
-require_once "DB.class.php";
+require_once "../DB.class.php";
 
 class Post extends DB
 {
