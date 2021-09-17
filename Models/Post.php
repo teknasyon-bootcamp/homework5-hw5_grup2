98b1bbda8d0f779eb21e01e769ead9bdfb6f3698
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-09-17 23:34:43 +0300

    Fix wrong file names. again...

--- Models/Post.php
+++ Models/Post.php
@@ -1,5 +1,5 @@
 <?php
-echo __DIR__.'/../DynamicDB.php';
+require_once __DIR__.'/../DynamicDB.php';
 
 class Post extends DynamicDB
 {
--- Models/Section.php
+++ Models/Section.php
@@ -1,5 +1,5 @@
 <?php
-echo __DIR__.'/../DynamicDB.php';
+require_once __DIR__.'/../DynamicDB.php';
 
 class Section extends DynamicDB
 {
