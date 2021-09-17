dfbad8ba515b9f2ad225de9477e50b27dcf195df
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-09-17 22:07:09 +0300

    Fix wrong property name

--- Models/Post.php
+++ Models/Post.php
@@ -3,7 +3,7 @@ require_once "DB.class.php";
 
 class Post extends DB
 {
-    public int $post_id;
+    public int $id;
     public int $book_id;
     public string $title = '';
     public string $content = '';
