095ad3db40a4a001527299116b369fd8bba9c0f9
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-09-20 13:55:14 +0300

    Fix bookOperations with new DB structure

--- Modules/BookOperation.php
+++ Modules/BookOperation.php
@@ -1,6 +1,12 @@
 <?php
+
+require_once __DIR__ . '/../Database/DynamicDB.php';
+
+
+$database = new DynamicDB;
+
 // Get all posts in my list 
-$books = Book::All();
+$books = $database->all('books');
 
 // Defines necessary variables for my post list and sets its default values
 $action = '';
@@ -8,43 +14,49 @@ $bookId = '';
 $name = '';
 $author = '';
 //get the value of action variable which was given in the url
-if(isset($_GET['action'])){ //isset function checks whether a variable is declared and is different than null. 
-    $action = $_GET['action']; 
+if (isset($_GET['action'])) { //isset function checks whether a variable is declared and is different than null. 
+    $action = $_GET['action'];
 }
-if(isset($_REQUEST['book'])){
-    $bookId = $_REQUEST['book']; 
+if (isset($_REQUEST['book'])) {
+    $bookId = $_REQUEST['book'];
 }
-if(isset($_POST['name'])){
+if (isset($_POST['name'])) {
     $name = $_POST['name'];
 }
-if(isset($_POST['author'])){
-    $author = $_POST['author']; 
+if (isset($_POST['author'])) {
+    $author = $_POST['author'];
 }
 /**
  * Performs create new post, update and delete existing post 
  * using the value of action variable in the url    
  */
+
 switch ($action) {
-    case 'edit' :   
-        $book = Book::find($bookId);
-        $book->name = $name;
-        $book->author = $author;
-        $book->update();
-        
+    case 'edit':
+
+        $values = [
+            'name' => $name,
+            'author' => $name,
+        ];
+
+        $database->update('books', $bookId, $values);
+
         // Sends a raw http header to the browser in a raw form
-        header("Refresh:0; url=index.php");  
+        header("Refresh:0; url=index.php");
         break;
     case 'create':
-        $book = new Book;
-        $book->name = $name;
-        $book->author = $author;
-        $book->create();
+        $values = [
+            'name' => $name,
+            'author' => $name,
+        ];
+
+        $database->create('books', $values);
+
         header("Refresh:0; url=index.php");
         break;
     case 'delete':
-        $book = Book::find($bookId);
-        $book->delete();
+        $database->delete('books', $bookId);
+
         header("Refresh:0; url=index.php");
         break;
 }
-?>
