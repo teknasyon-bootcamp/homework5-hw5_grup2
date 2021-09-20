c026324bf968ea9004b529920cbdd2cb1a3fe584
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-09-20 22:04:12 +0300

    Change db folder name

---
+++ app/Log/ILoggable.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Logger;
+
+interface ILoggable
+{
+    const EMERGENCY = 1;
+    const ALERT     = 2;
+    const CRITICAL  = 3;
+    const ERROR     = 4;
+    const WARNING   = 5;
+    const NOTICE    = 6;
+    const INFO      = 7;
+    const DEBUG     = 8;
+  
+    public function log(string $message,int $level):void;
+}
